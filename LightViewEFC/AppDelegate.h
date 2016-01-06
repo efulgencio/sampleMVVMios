62d1d05741216a7fec4f25b6a5fdf7972b0038fe
eduardo fulgencio <eduardofulgencio@macbook-pro-de-eduardo.local>
2016-01-06 18:19:33 +0100

    Initial Commit

---
+++ LightViewEFC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightViewEFC
+//
+//  Created by eduardo fulgencio on 6/1/16.
+//  Copyright © 2016 eduardo fulgencio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightViewEFC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightViewEFC
+//
+//  Created by eduardo fulgencio on 6/1/16.
+//  Copyright © 2016 eduardo fulgencio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
